c9f532ad510f28035b36958af16b0dd22519d2bd
Lei Wang <blueabysm@gmail.com>
2014-03-17 14:40:37 +0800

    调整代码格式

--- issue_1.c
+++ issue_1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {/* PASS */
+int main(int argc, char** argv) {
+  /* PASS */
   int array_1[3] = {0, 1, 2};
 
   /* PASS */
@@ -12,28 +13,16 @@ int main(int argc, char** argv) {/* PASS */
 
   /* PASS */
   int array_3[][2][3] = {
-    {
-      {1, 2}, {3}
-    },
-    {
-      {1, 2}, {1, 3}
-    },
-    {
-      {1, 2, 3}
-    }
+    {{1, 2}, {3}},
+    {{1, 2}, {1, 3}},
+    {{1, 2, 3}}
   };
 
   /* ERROR */
   int array_4[][][3] = {
-    {
-      {1, 2}, {3}
-    },
-    {
-      {1, 2}, {1, 3}
-    },
-    {
-      {1, 2, 3}
-    }
+    {{1, 2}, {3}},
+    {{1, 2}, {1, 3}},
+    {{1, 2, 3}}
   };
   return 0;
 }
